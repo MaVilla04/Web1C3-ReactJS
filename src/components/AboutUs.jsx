b628f6a2ce030c8214a933ed5b8d8a9864c88563
AndriwRC <randriw4@gmail.com>
2024-05-08 00:27:04 -0500

    muestra listado de empleados en about-us section

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function AboutUs() {
+  const [employees, setEmployees] = useState([]);
+  const getEmployees = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    setEmployees(data);
+  }
+  useEffect(() => {
+    getEmployees();
+  }, [])
+
   return (
-    <div>AboutUs</div>
+    <section>
+      <h3>Lista de Empleados:</h3>
+      <ul className='list-group'>
+        {
+          employees.map((employee) => (
+            <li
+              className='list-group-item'
+              key={employee.id}>
+              <Link to={`/about-us/${employee.id}`}>{employee.name} - {employee.email}</Link>
+            </li>
+          ))
+        }
+      </ul>
+    </section>
   )
 }
 
