a1334880537cf3a8350d24596ec197959d4f0c5a
AndriwRC <randriw4@gmail.com>
2024-05-13 15:12:49 -0500

    agrega pagina de 404 con estilos

--- src/components/Custom404.jsx
+++ src/components/Custom404.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
 
 function Custom404() {
   return (
-    <div>Custom404</div>
+    <section className="container my-5" style={{ minHeight: "75vh" }}>
+      <div className="container d-flex flex-wrap align-items-center justify-content-center px-5 bg-body-tertiary rounded-3 " >
+        <img src="src/assets/404.png" alt="404 image" className="bi mt-4 mb-3 img-fluid col-12 col-lg-5" />
+        <div className="text-center col-lg-7">
+          <h1 className="text-danger text-opacity-75">404</h1>
+          <p className="col-lg-8 mx-auto fs-5 text-muted">
+            Ups! It seems that you got lost...
+          </p>
+          <div className="d-inline-flex gap-2 mb-5">
+            <Link className="btn btn-danger my-3" to="/home" role="button">Return to Home</Link>
+          </div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
