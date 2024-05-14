383f06baf998ef30e212ac18e65b2e1248726785
AndriwRC <randriw4@gmail.com>
2024-05-14 13:23:09 -0500

    arregla direcciones en el deploy

--- src/components/Custom404.jsx
+++ src/components/Custom404.jsx
@@ -4,7 +4,7 @@ function Custom404() {
   return (
     <section className="container my-5 min-vh-100">
       <div className="container d-flex flex-wrap align-items-center justify-content-center px-5 bg-body-tertiary rounded-3 " >
-        <img src="src/assets/404.png" alt="404 image" className="bi mt-4 mb-3 img-fluid col-12 col-lg-5" />
+        <img src="./assets/404.png" alt="404 image" className="bi mt-4 mb-3 img-fluid col-12 col-lg-5" />
         <div className="text-center col-lg-7">
           <h1 className="text-danger text-opacity-75">404</h1>
           <p className="col-lg-8 mx-auto fs-5 text-muted">
